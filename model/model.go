6dced87fd54dde185e28e20c6157c460de32b158
Jesica <13519011@std.stei.itb.ac.id>
2022-10-15 20:45:36 +0700

    fix foreign key

--- model/model.go
+++ model/model.go
@@ -3,6 +3,8 @@ package model
 import (
 	"log"
 	"time"
+
+	// "github.com/satori/go.uuid"
 	"gorm.io/gorm"
 )
 
@@ -10,19 +12,20 @@ type Orders struct {
 	Order_id      uint      `json:"orderId" gorm:"primaryKey"`
 	Customer_name string    `json:"customerName"`
 	Ordered_at    time.Time `json:"orderedAt"`
-	Items         []Items   `gorm:"foreignKey:Item_id"`
+	MemberNumber  string
+	Items         []Items `gorm:"foreignKey:Item_code"`
 }
 
 type Items struct {
 	Item_id     uint   `json:"itemId" gorm:"primaryKey;autoIncrement"`
-	Item_code   string `json:"itemCode"`
+	Item_code   uint `json:"itemCode"`
 	Description string `json:"description"`
 	Quantity    uint   `json:"quantity"`
 	Order_id    uint   `json:"orderId" gorm:"foreignKey:Item_id"`
 }
 
-func (u *Orders) AfterCreate(tx *gorm.DB) (err error) {
+func (o *Orders) AfterCreate(tx *gorm.DB) (err error) {
 	log.Println("AFTER CREATE CALLED")
-	tx.Model(u.Items).Update("order_id", u.Order_id)
+	tx.Model(o.Items).Update("order_id", o.Order_id)
 	return
 }
